96ed2b3140b43837a8d0542fc3dba7024e1bec82
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2022-10-01 18:45:05 +0100

    Update lsh.py

--- anns/lsh.py
+++ anns/lsh.py
@@ -1,12 +1,12 @@
 import sys
 import csv
-from typing import Dict, List, Tuple
+from typing import List, Tuple
 import random
 from collections import defaultdict
 import math
 import time
 
-def read_data(file: str) -> List[Tuple(float)]:  # type hint syntax?
+def read_data(file: str) -> List[Tuple[float]]:
     dataset = []
     with open(file) as ip:
         reader = csv.reader(ip)
@@ -16,14 +16,14 @@ def read_data(file: str) -> List[Tuple(float)]:  # type hint syntax?
             dataset.append((float(x), float(y)))
     return dataset
 
-def generate_hyperplanes(L: int) -> List[Tuple(float)]:
+def generate_hyperplanes(L: int) -> List[Tuple[float]]:
     # generate L pairs of rand floats, these are just lines (d=2)
     planes = []
     for _ in range(L):
         planes.append((random.random() - 0.5, random.random() - 0.5)) # centered around origin
     return planes
 
-def calculate_dot_products(dataset: List[Tuple(float)], planes: List[Tuple(float)]) -> List[List[float]]:
+def calculate_dot_products(dataset: List[Tuple[float]], planes: List[Tuple[float]]) -> List[List[float]]:
     # dot product each point with the matrix of hyperplanes
     dot_products = []
     for (x, y) in dataset:
@@ -44,8 +44,8 @@ def obtain_hashes(dot_products: List[List[float]]) -> List[str]:
                 point_products[i] = 0
             else:
                 point_products[i] = 1
-    for i, point_products in enumerate(dot_products):
-        dot_products[i] = ''.join(point_products.astype(str))
+    for point_products in dot_products:
+        hashes.append(''.join(str(bit) for bit in point_products))
     return hashes
 
 def populate_hashtable(hashes: List[str]) -> dict:  # syntax for better type hint?
@@ -71,11 +71,11 @@ def search_kNN(hashtable: dict, hashes: List[str], k: int) -> dict:
             dist = hamming_distance(hash, hashes[candidate])
             distances.append((candidate, dist))
         distances.sort(key=lambda elt: elt[1])
-        knns[i] = set(distances[:k])  # not quite, as i'll have to add the tied ones too, but close enough
+        knns[i] = set([dist[0] for dist in distances[:k]])  # not quite, as i'll have to add the tied ones too, but close enough
     return knns
 
-def actual_search_kNN(dataset: List[Tuple(float)], k: int) -> dict:
-    def euclidean_distance(a: Tuple(float), b: Tuple(float)) -> float:
+def actual_search_kNN(dataset: List[Tuple[float]], k: int) -> dict:
+    def euclidean_distance(a: Tuple[float], b: Tuple[float]) -> float:
         (x1, y1) = a
         (x2, y2) = b
         return math.sqrt((x1-x2)**2 + (y1-y2)**2)
@@ -86,24 +86,25 @@ def actual_search_kNN(dataset: List[Tuple(float)], k: int) -> dict:
         for j, other in enumerate(dataset):
             if other == point:
                 continue
-            distances.append((i, j), euclidean_distance(point, other))
+            distances.append(((i, j), euclidean_distance(point, other)))
         distances.sort(key=lambda elt: elt[1])
-        knns[i] = set(distances[:k]) # again, do i consider other points with kdist?
+        knns[i] = set([dist[0][1] for dist in distances[:k]]) # again, do i consider other points with kdist?
     return knns
 
 def calculate_accuracy(approx_kNNs: dict, actual_kNNs: dict, n: int) -> dict:
     # calculate FPR and DR (?)
-    fpr_dr_fnr = defaultdict(tuple)
+    fp_tp_fn = defaultdict(tuple)
     for i in range(n):
-        fpr_dr_fnr[i] = (
+        fp_tp_fn[i] = (
+            # BAD
             # fpr = points in approx that aren't in actual
-            approx_kNNs[i].difference(actual_kNNs[i]),
+            len(approx_kNNs[i].difference(actual_kNNs[i])),
             # dr = intersection
-            approx_kNNs[i].intersection(actual_kNNs[i]),
+            len(approx_kNNs[i].intersection(actual_kNNs[i])),
             # fnr = points in actual but not in approx
-            actual_kNNs[i].difference(approx_kNNs[i])
+            len(actual_kNNs[i].difference(approx_kNNs[i]))
         )
-    return fpr_dr_fnr
+    return fp_tp_fn
 
 def main(L: int, k: int, file: str) -> tuple: # type
     dataset = read_data(file)
@@ -113,15 +114,18 @@ def main(L: int, k: int, file: str) -> tuple: # type
     dot_products = calculate_dot_products(dataset, planes)
     hashes = obtain_hashes(dot_products)
     hashtable = populate_hashtable(hashes)
-    approx_kNNs = search_kNN(hashtable, k)
+    approx_kNNs = search_kNN(hashtable, hashes, k)
     toc = time.perf_counter()
     elapsed = toc - tic
     actual_kNNs = actual_search_kNN(dataset, k)
-    accuracy = calculate_accuracy(approx_kNNs, actual_kNNs, n)
-    return elapsed, accuracy
+    fp_tp_fn = calculate_accuracy(approx_kNNs, actual_kNNs, n)
+    return elapsed, fp_tp_fn
 
 if __name__ == '__main__':
     [_, L, k, infile] = sys.argv
-    elapsed, accuracy = main(int(L), int(k), infile)
+    elapsed, fp_tp_fn = main(int(L), int(k), infile)
     print("in", elapsed, "seconds")
-    print("achieved results (best: (low, high, low))", accuracy)
+    print("achieved results")
+    # these results SUCK!
+    for (k, v) in fp_tp_fn.items():
+        print("point", k, ": (fpr:", v[0], ", dr:", v[1], ", fnr:", v[2] + ")")
